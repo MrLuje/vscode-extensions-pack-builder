e7c2959e61a55d1c2cea5f6c56c09d6557f1b024
MrLuje <ogmniluje@gmail.com>
2020-08-22 09:39:59 +0200

    move packFactory to async fs

--- src/node_async/fs.ts
+++ src/node_async/fs.ts
@@ -1,11 +1,14 @@
 import { promisify } from "util";
 import * as fs from "fs";
 
-const stat = promisify(fs.stat);
-const mkdir = promisify(fs.mkdir);
-const readFile = promisify(fs.readFile);
-const readdir = promisify(fs.readdir);
+const copyFile = promisify(fs.copyFile);
 const exists = promisify(fs.exists);
+const lstat = promisify(fs.lstat);
+const mkdir = promisify(fs.mkdir);
+const readdir = promisify(fs.readdir);
+const readFile = promisify(fs.readFile);
+const stat = promisify(fs.stat);
+const writeFile = promisify(fs.writeFile);
 
-const prfs = { stat, mkdir, readFile, readdir, exists };
+const prfs = { stat, mkdir, readFile, readdir, exists, copyFile, writeFile, lstat };
 export { prfs };
